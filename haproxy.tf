025713011a78a1bf34a70b2bc5ae91a29e2442fe
Arslan-SE <39010565+Arslan-SE@users.noreply.github.com>
2018-09-14 00:16:20 -0400

    Made comment for test.

--- haproxy.tf
+++ haproxy.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "haproxy" {
       user = "centos"
       private_key = "${var.private_key}"
   }
-
+  # Test Comment
   # TODO handle this with puppet
   provisioner "remote-exec" {
       inline = [
